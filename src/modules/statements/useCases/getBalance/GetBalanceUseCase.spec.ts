a04b141f24fa4cfafffdc8f565e272f9e902149c
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-05-04 14:32:55 -0300

    test: getBalanceUserUseCase

---
+++ src/modules/statements/useCases/getBalance/GetBalanceUseCase.spec.ts
@@ -0,0 +1,60 @@
+import { InMemoryUsersRepository } from "@modules/users/repositories/in-memory/InMemoryUsersRepository";
+import { AppError } from "@shared/errors/AppError";
+import { CreateUserUseCase } from "@modules/users/useCases/createUser/CreateUserUseCase";
+import { InMemoryStatementsRepository } from "@modules/statements/repositories/in-memory/InMemoryStatementsRepository";
+import { CreateStatementUseCase } from "../createStatement/CreateStatementUseCase";
+import { GetBalanceUseCase } from "./GetBalanceUseCase";
+import { GetBalanceError } from "./GetBalanceError";
+
+enum OperationType {
+  DEPOSIT = "deposit",
+  WITHDRAW = "withdraw",
+}
+
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let inMemoryStatementsRepository: InMemoryStatementsRepository;
+
+let createUserCase: CreateUserUseCase;
+let createStatementUseCase: CreateStatementUseCase;
+
+let getBalanceUseCase: GetBalanceUseCase;
+
+describe("Get Balance", () => {
+  beforeEach(async () => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    inMemoryStatementsRepository = new InMemoryStatementsRepository();
+
+    createUserCase = new CreateUserUseCase(inMemoryUsersRepository);
+    createStatementUseCase = new CreateStatementUseCase(
+      inMemoryUsersRepository,
+      inMemoryStatementsRepository
+    );
+
+    getBalanceUseCase = new GetBalanceUseCase(
+      inMemoryStatementsRepository,
+      inMemoryUsersRepository
+    );
+  });
+
+  it("should be able to get the user balance", async () => {
+    const user = await createUserCase.execute({
+      name: "XXX",
+      email: "XXX@mail.com",
+      password: "XXXX",
+    });
+
+    const userBalance = await getBalanceUseCase.execute({
+      user_id: user.id,
+    });
+
+    expect(userBalance.balance).toBe(0);
+  });
+
+  it("should not be able to get the user balance for non exists user", async () => {
+    expect(async () => {
+      await getBalanceUseCase.execute({
+        user_id: "2222",
+      });
+    }).rejects.toBeInstanceOf(GetBalanceError);
+  });
+});

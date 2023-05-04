7a311ef12f2c01ee511b707e96d2f58189c07e3c
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-05-04 01:29:28 -0300

    test: CreateStatmentUseCase

--- src/modules/statements/useCases/createStatement/CreateStatmentUseCase.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatmentUseCase.spec.ts
@@ -3,6 +3,7 @@ import { AppError } from "@shared/errors/AppError";
 import { CreateStatementUseCase } from "./CreateStatementUseCase";
 import { CreateUserUseCase } from "@modules/users/useCases/createUser/CreateUserUseCase";
 import { InMemoryStatementsRepository } from "@modules/statements/repositories/in-memory/InMemoryStatementsRepository";
+import { CreateStatementError } from "./CreateStatementError";
 
 enum OperationType {
   DEPOSIT = "deposit",
@@ -27,21 +28,32 @@ describe("Create Statement", () => {
     );
   });
 
-  it("should be able to create deposit a statement", async () => {
-    const user = await inMemoryUsersRepository.create({
-      name: "Test user",
-      email: "test@email.com",
-      password: "123456",
+  it("should be able to create a new deposit statement", async () => {
+    const user = await createUserCase.execute({
+      name: "XXX",
+      email: "XXX@mail.com",
+      password: "XXXX",
     });
 
     const statement = await createStatementUseCase.execute({
       user_id: user.id,
       type: OperationType.DEPOSIT,
       amount: 500.0,
-      description: "Test deposit statement",
+      description: "statement test",
     });
 
     expect(statement).toHaveProperty("id");
     expect(statement.amount).toBe(500.0);
+  });
+
+  it("should not be able to create a new deposit statement with non exists user", async () => {
+    expect(async () => {
+      await createStatementUseCase.execute({
+        user_id: "123",
+        type: OperationType.DEPOSIT,
+        amount: 500.0,
+        description: "statement test",
+      });
+    }).rejects.toBeInstanceOf(CreateStatementError.UserNotFound);
   });
 });

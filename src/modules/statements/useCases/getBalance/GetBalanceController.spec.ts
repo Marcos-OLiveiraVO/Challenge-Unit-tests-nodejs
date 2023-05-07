27c245d782c199c81ddfe97940849055dc97ac24
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-05-07 00:19:23 -0300

    test: GetBalanceController

---
+++ src/modules/statements/useCases/getBalance/GetBalanceController.spec.ts
@@ -0,0 +1,58 @@
+import request from "supertest";
+
+import { app } from "../../../../app";
+import { dataSource } from "../../../../database/data-source";
+
+import { v4 as uuidV4 } from "uuid";
+import { hashSync } from "bcryptjs";
+
+const id = uuidV4();
+const password = hashSync("admin", 8);
+
+describe("Create Statement Controller", () => {
+  beforeAll(async () => {
+    await dataSource.initialize();
+    await dataSource.runMigrations();
+
+    await dataSource.query(
+      `INSERT INTO USERS(id, name, email, password, created_at, updated_at)
+      VALUES ('${id}', 'admin', 'admin@mail.com', '${password}', 'now()', 'now()')`
+    );
+  });
+
+  afterAll(async () => {
+    await dataSource.dropDatabase();
+    await dataSource.destroy();
+  });
+
+  it("should be able to get balance", async () => {
+    const responseToken = await request(app).post("/api/v1/sessions").send({
+      email: "admin@mail.com",
+      password: "admin",
+    });
+
+    const { token } = responseToken.body;
+
+    await request(app)
+      .post("/api/v1/statements/deposit")
+      .send({
+        amount: 100,
+        description: "Deposit test",
+      })
+      .set({ Authorization: `Bearer ${token}` });
+
+    await request(app)
+      .post("/api/v1/statements/withdraw")
+      .send({
+        amount: 50,
+        description: "Withdraw test",
+      })
+      .set({ Authorization: `Bearer ${token}` });
+
+    const response = await request(app)
+      .get("/api/v1/statements/balance")
+      .set({ Authorization: `Bearer ${token}` });
+
+    expect(response.status).toBe(200);
+  });
+});
